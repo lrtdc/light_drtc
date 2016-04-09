0354801c5e41d77cb3fa94f79a3455daa7adc383
a <wang_cheng_guang@163.com>
2016-04-09 19:15:02 +0800

    任务管理节点将之前仅支持双主模式 更改为 支持多主模式(可以支持2个以上任务管理节点)

--- src/main/java/org/light/rtc/timer/MqDoTimer.java
+++ src/main/java/org/light/rtc/timer/MqDoTimer.java
@@ -8,46 +8,71 @@ import org.light.rtc.util.ConfigProperty;
 import org.light.rtc.util.Constants;
 import org.light.rtc.util.Murmurs;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 import java.util.TimerTask;
+import java.util.Map.Entry;
 
 public class MqDoTimer extends TimerTask {
-	protected static ConcurrentLinkedQueue<String> firstStreamLogs = new ConcurrentLinkedQueue<String>();
-	protected static ConcurrentLinkedQueue<String> secondStreamLogs = new ConcurrentLinkedQueue<String>();
+	protected List<ConcurrentLinkedQueue<String>> dataStreamList = new ArrayList<ConcurrentLinkedQueue<String>>();
 	
 	private String[] adminNodeHosts = Constants.adminNodeHosts.split(",");
-	private int adminNodeNum = adminNodeHosts.length;
+	private int adminNodeNum;
 	private ConcurrentHashMap<Integer,LrtdcClient> adminNodeClientMap;
 	private Murmurs mmHash = new Murmurs();
 	
 	public MqDoTimer(){
 		if(adminNodeHosts!=null){
+			adminNodeNum = adminNodeHosts.length;
 			adminNodeClientMap = new ConcurrentHashMap<Integer,LrtdcClient>();
 			for(int i=0; i<adminNodeNum; i++){
 				String[] tmpHostIp = adminNodeHosts[i].split(":");
 				adminNodeClientMap.put(i, new LrtdcClient(tmpHostIp[0], Integer.parseInt(tmpHostIp[1])));
+				dataStreamList.add(new ConcurrentLinkedQueue<String>());
 			}
 		}
 	}
 	
 	public void parseMqText(String routKey, String mqText){
 		int adminId = (int)(mmHash.getGuavaMurmurs(routKey)%adminNodeNum);
-		if(adminId==0){
-			firstStreamLogs.add(mqText);
-		}else{
-			secondStreamLogs.add(mqText);
+		this.dataStreamList.get(adminId).add(mqText);
+	}
+	
+	public List<Integer> getAbleAdminNodeIds(){
+		List<Integer> ableJobNodeIds = new ArrayList<Integer>();
+		for(Entry<Integer,LrtdcClient> item : adminNodeClientMap.entrySet()){
+			if(item.getValue().getHealthStatus()==1){
+				ableJobNodeIds.add(item.getKey());
+			}
 		}
+		return ableJobNodeIds;
 	}
 
 	@Override
 	public void run() {
 		try{
-			int firstQuNum = firstStreamLogs.size();
-			int secondQuNum = secondStreamLogs.size();
-			if(firstQuNum>0 || secondQuNum>0){
-				System.out.println("AdminNodeId_1: "+firstQuNum+" AdminNodeId_2: "+secondQuNum);
-				new Thread(new BatchDoLog(firstQuNum, secondQuNum)).start();
+			int tmpNum = -1;
+			Map<Integer,Integer> dataQuNums = new HashMap<Integer,Integer>();
+			for(int i=0; i<dataStreamList.size(); i++){
+				tmpNum = dataStreamList.get(i).size();
+				if(tmpNum>0){
+					dataQuNums.put(i, tmpNum);
+				}
+			}
+			if(dataQuNums.size()>0){
+				System.out.println(ConfigProperty.getCurDateTime()+" currently will be doing adminNode and logNum : "+dataQuNums);
+				List<Integer> enableANIds = this.getAbleAdminNodeIds();
+				if(enableANIds.size()>0){
+					for(Entry<Integer,Integer> dqnItem : dataQuNums.entrySet()){
+						new Thread(new BatchDoLog(dqnItem.getKey(), dqnItem.getValue(), enableANIds)).start();
+					}
+				}else{
+					System.out.println(ConfigProperty.getCurDateTime()+" currently there is no enable adminNode for receving stream logs");
+				}
 			}else{
 				System.out.println(ConfigProperty.getCurDateTime()+"\t 当前时刻无消息 ");
 			}
@@ -57,39 +82,29 @@ public class MqDoTimer extends TimerTask {
 	}
 	
 	protected class BatchDoLog implements Runnable{
-		private int firstNum;
-		private int secondNum;
+		private int adminId;
+		private int logNum;
+		private List<Integer> enableAnIds;
 		
-		public BatchDoLog(int firstNum, int secondNum){
-			this.firstNum = firstNum;
-			this.secondNum = secondNum;
+		public BatchDoLog(int adminId, int logNum, List<Integer> enableAdminIds){
+			this.adminId = adminId;
+			this.logNum = logNum;
+			this.enableAnIds = enableAdminIds;
 		}
 
+		private Random rand = new Random();
+		
 		@Override
 		public void run() {
-			List<String> tmpLogs = null;
-			if(firstNum>0){
-				tmpLogs = new LinkedList<String>();
-				for(int i=0;i<firstNum;i++){
-					tmpLogs.add(firstStreamLogs.poll());
-				}
-				if(adminNodeClientMap.get(0).getHealthStatus()==1){
-					adminNodeClientMap.get(0).addMqinfoToAdminQu(tmpLogs);
-				}else{
-					adminNodeClientMap.get(1).addMqinfoToAdminQu(tmpLogs);
-				}
+			List<String> tmpLogs = new LinkedList<String>();
+			ConcurrentLinkedQueue<String> tmpQu = dataStreamList.get(adminId);
+			for(int i=0;i<logNum;i++){
+				tmpLogs.add(tmpQu.poll());
 			}
-			if(secondNum>0){
-				tmpLogs = new LinkedList<String>();
-				for(int i=0;i<secondNum;i++){
-					tmpLogs.add(secondStreamLogs.poll());
-				}
-				if(adminNodeClientMap.get(1).getHealthStatus()==1){
-					adminNodeClientMap.get(1).addMqinfoToAdminQu(tmpLogs);
-				}else{
-					adminNodeClientMap.get(0).addMqinfoToAdminQu(tmpLogs);
-				}
+			if(!enableAnIds.contains(adminId)){
+				adminId = enableAnIds.get(rand.nextInt(enableAnIds.size()));
 			}
+			adminNodeClientMap.get(adminId).addMqinfoToAdminQu(tmpLogs);
 		}
 	}
 
