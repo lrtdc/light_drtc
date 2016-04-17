37389146beb6d4cdbbbef823fcc20906bd12fea7
a <wang_cheng_guang@163.com>
2016-04-17 09:38:38 +0800

    增加AN和CN整合（如果开发者选用Kafka或RabbitMQ）

---
+++ src/main/java/org/light/rtc/admin/AdminNodeRabbitMqRun.java
@@ -0,0 +1,76 @@
+package org.light.rtc.admin;
+
+import java.util.Timer;
+
+import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TServerTransport;
+import org.apache.thrift.transport.TTransportException;
+import org.light.rtc.api.TDssService;
+import org.light.rtc.base.StreamLogParser;
+import org.light.rtc.mq.RabbitMqCollect;
+import org.light.rtc.service.RtdcAdminService;
+import org.light.rtc.timer.AdminNodeTimer;
+import org.light.rtc.util.ConfigProperty;
+import org.light.rtc.util.Constants;
+
+public class AdminNodeRabbitMqRun {
+
+	/**
+	 * your self defending function for parsing your data stream logs
+	 * @param slp
+	 */
+	public void setSteamParser(StreamLogParser slp){
+		AdminNodeTimer.setStreamLogParser(slp);
+	}
+	
+	private class DataCollect implements Runnable{
+		@Override
+		public void run() {
+			RabbitMqCollect rmc = new RabbitMqCollect();
+			rmc.run();
+		}
+	}
+	
+	public void run(){
+		this.adminJobTImer();
+		
+		new Thread(new DataCollect()).start();
+		
+		RtdcAdminService rss = new RtdcAdminService();
+		TDssService.Processor<RtdcAdminService> tp = new TDssService.Processor<RtdcAdminService>(rss);
+		TServerTransport serverTransport = null;
+		try {
+			serverTransport = new TServerSocket(Constants.adminNodePort);
+		} catch (TTransportException e) {
+			e.printStackTrace();
+		}
+		TThreadPoolServer.Args tArgs = new TThreadPoolServer.Args(serverTransport);
+		tArgs.maxWorkerThreads(1000);
+		tArgs.minWorkerThreads(10);
+		tArgs.processor(tp);
+		TCompactProtocol.Factory portFactory =  new TCompactProtocol.Factory();
+		tArgs.protocolFactory(portFactory);
+		
+		TServer tServer = new TThreadPoolServer(tArgs);
+		System.out.println(ConfigProperty.getCurDateTime()+"......轻量级实时计算框架任务管理节点(通过RabbitMq整合CN)服务启动......"); 
+		tServer.serve();
+		serverTransport.close();
+		tServer.stop();
+		System.out.println("......轻量级实时计算框架任务管理节点(通过RabbitMq整合CN)服务停止......");
+	}
+	
+	public void adminJobTImer(){
+		AdminNodeTimer ant = new AdminNodeTimer();
+		Timer daoTimer = new Timer();
+		daoTimer.schedule(ant, 100, 1000 * Constants.rtcPeriodSeconds);
+		 try {
+			 Thread.sleep(100);
+		 } catch (InterruptedException e) {
+			e.printStackTrace();
+		 }
+	}
+	
+}
--- src/test/java/org/light/ldrtc/test/AdminNodeServer.java
+++ src/test/java/org/light/ldrtc/test/AdminNodeServer.java
@@ -1,11 +1,15 @@
 package org.light.ldrtc.test;
 
 import org.light.ldrtc.parser.LogParser;
+//import org.light.rtc.admin.AdminNodeKafkaRun;
+//import org.light.rtc.admin.AdminNodeRabbitMqRun;
 import org.light.rtc.admin.AdminNodeRun;
 
 public class AdminNodeServer {
 
 	public void run(){
+//		AdminNodeRabbitMqRun anr = new AdminNodeRabbitMqRun();
+//		AdminNodeKafkaRun anr = new AdminNodeKafkaRun();
 		AdminNodeRun anr = new AdminNodeRun();
 		anr.setSteamParser(new LogParser());//配置解析日志数据流
 		anr.run();
